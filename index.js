880e65924dc9ea6e7cc42789bedfa1d53081dcee
janay <janayby@gmail.com>
2023-05-12 11:17:45 +0300

    working to connect to db

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-yny-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.querySelector("#shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
-    console.log(inputValue)
+    push(shoppingListInDB, inputValue)
+    // appendItemToShoppingListEl(inputValue)
+    cleanInputFieldEl()
+    console.log(`${inputValue} add to database`)
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+    let shoppingListArray = Object.values(snapshot.val())
+
+    shoppingListEl.innerHTML = ""
+
+    for (let i = 0; i < shoppingListArray.length; i++) {
+        const element = shoppingListArray[i];
+        appendItemToShoppingListEl(element)
+    }
+
+})
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
+
+function cleanInputFieldEl() {
+    inputFieldEl.value = ""
+}
